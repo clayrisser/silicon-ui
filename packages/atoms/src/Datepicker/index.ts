883c6df2204d0bff6cc7aba6d23e1ecfddb1cdd7
Praveen <praveenkumarvyk@gmail.com>
2020-07-27 12:42:27 +0000

    Added datepicker

---
+++ packages/atoms/src/Datepicker/index.ts
@@ -0,0 +1,5 @@
+import Datepicker from './Datepicker';
+import { DatepickerProps } from './datepickerProps';
+
+export { DatepickerProps };
+export default Datepicker;
--- packages/atoms/src/index.ts
+++ packages/atoms/src/index.ts
@@ -5,12 +5,15 @@ import Image, { ImageProps } from './Image';
 import Input, { InputProps } from './Input';
 import Item, { ItemProps } from './Item';
 import Text, { TextProps } from './Text';
+import Datepicker, { DatepickerProps } from './Datepicker';
 
 export {
   Box,
   BoxProps,
   Button,
   ButtonProps,
+  Datepicker,
+  DatepickerProps,
   Form,
   FormProps,
   Image,
