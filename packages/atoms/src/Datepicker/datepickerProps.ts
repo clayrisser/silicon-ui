ae233376f837986171dd5299aed158f512927552
Jam Risser <jam@codejam.ninja>
2020-08-12 17:09:48 +0530

    modified datepicker  knobs

--- packages/atoms/src/Datepicker/datepickerProps.ts
+++ packages/atoms/src/Datepicker/datepickerProps.ts
@@ -39,6 +39,8 @@ export interface CustomDatepickerProps {
   max?: string;
   min?: string;
   required?: boolean;
+  onDateChange?: (event: any) => void;
+  // onClickOutsile?: (event :any) => void;
 }
 
 export interface DatepickerProps
