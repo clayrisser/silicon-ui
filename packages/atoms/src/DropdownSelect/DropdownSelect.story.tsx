d0e54027eed768c5196a37c3409c2423ffb724ef
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2020-08-21 13:49:13 +0000

    Fixed text in native

--- packages/atoms/src/DropdownSelect/DropdownSelect.story.tsx
+++ packages/atoms/src/DropdownSelect/DropdownSelect.story.tsx
@@ -27,7 +27,11 @@ storiesOf('Dropdown', module)
     <Wrapper>
       <DropdownSelect
         onPress={action('onPress')}
+<<<<<<< Updated upstream
         backgroundColor={color('backgroundColor', 'primary')}
+=======
+        backgroundColor={text('backgroundColor', 'inverseText')}
+>>>>>>> Stashed changes
         borderRadius={number('border-radius', 0)}
         borderColor={color('border-color', '')}
         borderWidth={number('border-width', 2)}
