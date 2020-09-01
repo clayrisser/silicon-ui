673544996b6ad71194b174ac8ef7e8f48e6bfc34
Jam Risser <jam@codejam.ninja>
2020-09-01 18:31:45 +0530

    unit testing

--- packages/atoms/tests/Input/Input.test.tsx
+++ packages/atoms/tests/Input/Input.test.tsx
@@ -2,10 +2,52 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 // import { render, fireEvent } from '@testing-library/react';
 import Input from '../../src/Input';
+import { autoContrast } from '../../src/hooks/useColor';
+import { lineHeight, fontWeight, borderWidth } from 'styled-system';
 
 describe('<Input />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Input />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('<Input />', () => {
+  it('renders correctly', () => {
+    const fontSize = 16;
+    const bg = 'white';
+    const color = 'black';
+    const height = 90;
+    const padding = 9;
+    const width = 600;
+    const border = 1;
+    const borderStyle = 'solid';
+    const borderColor = 'green';
+    const backgroundColor = 'rerd';
+    const lineHeight = 2;
+    const autoContrast = true;
+    const fontWeight = 2;
+    const borderWidth = 1;
+    const tree = renderer
+      .create(
+        <Input
+          fontSize={fontSize}
+          fontWeight={fontWeight}
+          borderWidth={borderWidth}
+          bg={bg}
+          color={color}
+          height={height}
+          p={padding}
+          border={border}
+          width={width}
+          borderStyle={borderStyle}
+          borderColor={borderColor}
+          backgroundColor={backgroundColor}
+          autoContrast={autoContrast}
+          lineHeight={lineHeight}
+        />
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
