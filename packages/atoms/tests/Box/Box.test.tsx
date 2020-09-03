64b706e66ce79414e335144c2469218325823239
Jam Risser <jam@codejam.ninja>
2020-09-03 15:43:18 +0530

    unit testing

--- packages/atoms/tests/Box/Box.test.tsx
+++ packages/atoms/tests/Box/Box.test.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 // import { render, fireEvent } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Box from '../../src/Box/Box';
+import { act } from '@testing-library/react-native';
+import { create } from 'react-test-renderer';
 
 describe('<Box />', () => {
   it('renders correctly', () => {
@@ -66,4 +68,40 @@ describe('<Box with props />', () => {
   //   fireEvent.mouseDown(container.querySelector('HTMLDiv')!);
   //   expect(handleMouseDown).toBe(undefined);
   // });
+});
+
+describe('<Box with events />', () => {
+  it('renders correctly with events', () => {
+    const onPress = jest.fn();
+    const onMouseDown = jest.fn();
+    const onMouseEnter = jest.fn();
+    const onBlur = jest.fn();
+    const onMouseMove = jest.fn();
+    const onMouseLeave = jest.fn();
+    const onMouseUp = jest.fn();
+
+    let component: any;
+    act(() => {
+      component = create(
+        <Box
+          onPress={onPress}
+          onBlur={onBlur}
+          onMouseEnter={onMouseEnter}
+          onMouseMove={onMouseMove}
+          onMouseLeave={onMouseLeave}
+          onMouseUp={onMouseUp}
+          onMouseDown={onMouseDown}
+        />
+      );
+    });
+    const rootInstance: any = component.root;
+    const box = rootInstance.findAllByType(Box);
+    expect(box.length).toBe(1);
+    expect(box[0].props.onPress()).toBe(undefined);
+    expect(box[0].props.onMouseDown()).toBe(undefined);
+    expect(box[0].props.onMouseEnter()).toBe(undefined);
+    expect(box[0].props.onBlur()).toBe(undefined);
+    expect(box[0].props.onMouseLeave()).toBe(undefined);
+    expect(box[0].props.onMouseUp()).toBe(undefined);
+  });
 });
